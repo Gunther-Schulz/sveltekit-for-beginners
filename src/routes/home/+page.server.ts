fc78b12a8fb07381ebb54d5eaf304bee1fc0c6a6
freedumb2000 <freedumb2000@example.com>
2022-10-22 12:48:02 +0200

    wip

--- src/routes/home/+page.server.ts
+++ src/routes/home/+page.server.ts
@@ -9,8 +9,34 @@ import {
 } from '$lib/utils/prisma'
 
 export const load: PageServerLoad = async () => {
-	const tweets = await getTweets()
-
+	// const tweets = await getTweets()
+	const tweets = JSON.parse(`
+	[
+		{
+		  "id": 34,
+		  "content": "",
+		  "likes": 0,
+		  "posted": "Sep 19, 2022",
+		  "url": "3544f46d10f33",
+		  "avatar": "/profile/matia/avatar.webp",
+		  "handle": "@joyofcodedev",
+		  "name": "matia",
+		  "liked": false
+		},
+		{
+		  "id": 33,
+		  "content": "gtrgr",
+		  "likes": 0,
+		  "posted": "Sep 19, 2022",
+		  "url": "59cb25be68ae2",
+		  "avatar": "/profile/matia/avatar.webp",
+		  "handle": "@joyofcodedev",
+		  "name": "matia",
+		  "liked": false
+		}
+	  ]
+	`)
+	console.log(tweets)
 	if (!tweets) {
 		throw error(400, 'No Tweets Found')
 	}
